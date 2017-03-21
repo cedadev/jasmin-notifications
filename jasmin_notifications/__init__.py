fcf02a3c38b84bf0a25f31ecd0086126c737fdc2
Matt Pryor <mkjpryor-stfc@users.noreply.github.com>
2017-03-21 20:15:26 +0000

    Version bump

--- jasmin_notifications/__init__.py
+++ jasmin_notifications/__init__.py
@@ -7,7 +7,7 @@ This app provides generic functionality for sending and showing notifications.
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 
 default_app_config = '{}.apps.AppConfig'.format(__name__)
