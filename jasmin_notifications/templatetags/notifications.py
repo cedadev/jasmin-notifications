72e4cfd8e14ae6d587940d6404937695cb4ecf9f
Alex Manning <alexander.manning@stfc.ac.uk>
2022-06-30 15:29:50 +0100

    Add a 'Clear All Notifications' Button

--- jasmin_notifications/templatetags/notifications.py
+++ jasmin_notifications/templatetags/notifications.py
@@ -8,14 +8,14 @@ __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 from django import template
 from django.urls import reverse
 
-from ..models import UserNotification
 from ..helpers import notification_context
-
+from ..models import UserNotification
 
 register = template.Library()
 
+
 @register.inclusion_tag(
-    'jasmin_notifications/notification_dropdown.html', takes_context = True
+    "jasmin_notifications/notification_dropdown.html", takes_context=True
 )
 def notification_dropdown(context):
     """
@@ -27,20 +27,19 @@ def notification_dropdown(context):
     in scope. The context for each notification will be as returned by
     :py:func:`~.helpers.notification_context`.
     """
-    # Get the logged in user from the context
-    user = context.get('user')
+    # Get the logged in user from the context
+    user = context.get("user")
     if user and user.is_authenticated:
         # Get the unread notifications for display for the user
-        notifications = UserNotification.objects.filter(notification_type__display = True,
-                                                        user = user,
-                                                        followed_at__isnull = True)  \
-                                                .order_by('-created_at')
+        notifications = UserNotification.objects.filter(
+            notification_type__display=True, user=user, followed_at__isnull=True
+        ).order_by("-created_at")
     else:
         notifications = []
     # Convert the notifications into a more friendly dict for rendering
-    notifications = [ notification_context(n) for n in notifications ]
+    notifications = [notification_context(n) for n in notifications]
     # Add in any extra notifications from the context
-    notifications.extend(context.get('notifications_extra', []))
+    notifications.extend(context.get("notifications_extra", []))
     return {
-        'notifications' : notifications,
+        "notifications": notifications,
     }
--- jasmin_notifications/views.py
+++ jasmin_notifications/views.py
@@ -5,16 +5,17 @@ Module defining views for the JASMIN notifications app.
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 
-from django.views.decorators.http import require_safe
+import django.shortcuts
+import django.views.decorators.http
 from django import http
+from django.contrib.auth.views import redirect_to_login
 from django.shortcuts import redirect
 from django.utils import timezone
-from django.contrib.auth.views import redirect_to_login
 
 from .models import Notification, UserNotification
 
 
-@require_safe
+@django.views.decorators.http.require_safe
 def follow(request, uuid):
     """
     Handler for ``/<uuid>/``.
@@ -25,25 +26,33 @@ def follow(request, uuid):
     redirecting to the link.
     """
     # First, try to find a notification with the UUID
-    notification = Notification.objects.filter(uuid = uuid).first()
+    notification = Notification.objects.filter(uuid=uuid).first()
     if not notification:
         raise http.Http404("Notification does not exist")
     if isinstance(notification, UserNotification):
-        # For user notifications, we require an authenticated user
+        # For user notifications, we require an authenticated user
         if not request.user.is_authenticated:
             return redirect_to_login(request.path)
-        # The notification must be for the logged-in user
+        # The notification must be for the logged-in user
         if request.user != notification.user:
             raise http.Http404("Notification does not exist")
         # Update the followed_at time for all the notifications for the same user
-        # and link
-        UserNotification.objects.filter(link = notification.link,
-                                        user = notification.user,
-                                        followed_at__isnull = True)  \
-                                .update(followed_at = timezone.now())
+        # and link
+        UserNotification.objects.filter(
+            link=notification.link, user=notification.user, followed_at__isnull=True
+        ).update(followed_at=timezone.now())
     else:
         # For email notifications, just update this notification
         if not notification.followed_at:
             notification.followed_at = timezone.now()
         notification.save()
     return redirect(notification.link)
+
+
+@django.views.decorators.http.require_POST
+def clear_all(request):
+    """Clear all of a user's displayable notifications."""
+    UserNotification.objects.filter(
+        user=request.user, followed_at__isnull=True, notification_type__display=True
+    ).update(followed_at=timezone.now())
+    return redirect(request.META.get("HTTP_REFERER", "/"))
