06e99970af8054d1b2f7cc802a9f8acebbb51865
Alex Manning <alexander.manning@stfc.ac.uk>
2022-09-20 09:24:51 +0100

    Add a 'Clear All Notifications' Button (#6)

--- jasmin_notifications/urls.py
+++ jasmin_notifications/urls.py
@@ -5,15 +5,16 @@ URL configuration for the JASMIN notifications app.
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 
-from django.conf.urls import url, include
+import django.urls
 
 from . import views
 
-app_name = 'jasmin_notifications'
+app_name = "jasmin_notifications"
 urlpatterns = [
-    url(
-        r'^(?P<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',
+    django.urls.re_path(
+        r"^(?P<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$",
         views.follow,
-        name = 'follow'
+        name="follow",
     ),
+    django.urls.path("clear_all/", views.clear_all, name="clear_all"),
 ]
