c78ef95bb888227f1c228f48742c7e14852c710e
Alex Manning <alexander.manning@stfc.ac.uk>
2023-04-28 15:24:27 +0100

    Tweaks to clearjasminnotifications command.

--- jasmin_notifications/management/commands/clearjasminnotifications.py
+++ jasmin_notifications/management/commands/clearjasminnotifications.py
@@ -1,6 +1,7 @@
 import datetime
 
 import django.core.management.base
+import django.utils
 
 from ... import models
 
@@ -12,19 +13,21 @@ class Command(django.core.management.base.BaseCommand):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.clean_followed = datetime.datetime.now() - datetime.timedelta(days=365)  # 1 year.
-        self.clean_unfollowed = datetime.datetime.now() - datetime.timedelta(days=1826)  # 5 years.
+        self.clean_followed = django.utils.timezone.now() - datetime.timedelta(days=365)  # 1 year.
+        self.clean_unfollowed = django.utils.timezone.now() - datetime.timedelta(
+            days=1826
+        )  # 5 years.
 
     def handle(self, *args, **options):
         """Delete old notifications."""
         old_followed_notifications = models.Notification.objects.filter(
             followed_at__isnull=False, followed_at__lt=self.clean_followed
         )[:100]
-        countf, _ = old_followed_notifications.delete()
-        self.stdout.write(f"Deleted {countf} old followed notifications")
+        if old_followed_notifications:
+            old_followed_notifications.delete()
 
         old_notifications = models.Notification.objects.filter(
             created_at__lt=self.clean_unfollowed
         )[:100]
-        counto, _ = old_notifications.delete()
-        self.stdout.write(f"Deleted {counto} old.")
+        if old_notifications:
+            old_notifications.delete()
